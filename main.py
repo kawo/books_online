6314edc10b3351100ec7b589aeb7ee49ac287947
Kevin Audebrand <kevin.audebrand@gmail.com>
2021-05-28 18:34:27 +0200

    prompting user to choose a category

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from app.scrap_book import ScrapBook
 from app.scrap_navigation import ScrapCategory
 
+# BUG: out of range with Default cat
+
 
 def main():
 
     # grabing dict of categories
     cat_list = ScrapCategory.scrapCategoryList()
-    cat_url = cat_list["Nonfiction"]
+    for name, url in cat_list.items():
+        print(f"{name}")
+    cat_name = input(f"Type a category from the list above: ")
+    cat_url = cat_list[cat_name]
 
     # grabing list of books from one category
     book_list = ScrapCategory()
